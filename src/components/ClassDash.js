a22c1896fdbeacd65bc13cd0187c9f74df50396c
T-Cal <tcalcuttawala@gmail.com>
2021-06-01 17:06:26 -0400

    updated class add form

--- src/components/ClassDash.js
+++ src/components/ClassDash.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import { ListGroup, Tab, Row, Col, Modal, Form } from "react-bootstrap";
+import { ListGroup, Tab, Row, Col, Modal, Form, Button } from "react-bootstrap";
 import ClassInfo from "./ClassInfo";
 
-function ClassDash({ classes }) {
+function ClassDash({ classes, teachers }) {
   const [modal, setModal] = useState(false);
   const [className, setClassName] = useState("");
   const [grade, setGrade] = useState(0);
@@ -12,6 +12,7 @@ function ClassDash({ classes }) {
     setModal(true);
     e.preventDefault();
   };
+
   return (
     <Tab.Container defaultActiveKey="#link0">
       <ListGroup>
@@ -34,46 +35,99 @@ function ClassDash({ classes }) {
           <Modal.Title>Add a Class</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-			<Form.Label>Class ID</Form.Label>
-          <Form.Control
-            placeholder="TJE#"
-            onChange={(e) => {
-              setClassName(e.target.value);
+          <Form
+            onSubmit={(e) => {
+              const classID= e.target.classID.value;
+			  let gl = "";
+				e.preventDefault();
+              //const formData = new FormData(e.target);
+              //formDataObj = Object.fromEntries(formData.entries());
+              //console.log(e.target.group1);
+			  for(let radio in e.target.group1){
+				  if(e.target.group1[radio].checked) {
+					  gl = parseInt(radio) + 1;
+				  }
+			  }
+			  const teacher = e.target.teacher.value;
+
+			  console.log(classID);
+			  console.log(gl);
+			  console.log(teacher);
+
             }}
-          ></Form.Control><br/>
-          <div key={`inline-${'radio'}`}>
-            <Form.Label>Grade Level</Form.Label><br/>
-			<Form.Check
-              inline
-              label="1"
-              name="group1"
-              type='radio'
-            />
-            <Form.Check
-              inline
-              label="2"
-              name="group1"
-              type='radio'
-            />
-			<Form.Check
-              inline
-              label="3"
-              name="group1"
-              type='radio'
-            />
-			<Form.Check
-              inline
-              label="4"
-              name="group1"
-              type='radio'
-            />
-			<Form.Check
-              inline
-              label="5"
-              name="group1"
-              type='radio'
-            />
-          </div>
+          >
+            <Form.Label>Class ID</Form.Label>
+            <Form.Control
+              hasValidation
+              required
+              placeholder="TJE#"
+              name="classID"
+            ></Form.Control>
+            <br />
+            <div key={`inline-${"radio"}`}>
+              <Form.Label>Grade Level</Form.Label>
+              <br />
+              <Form.Group name="radios">
+                <Form.Check
+                  required
+                  inline
+                  label="1"
+                  name="group1"
+                  type="radio"
+                  id="1"
+				  checked
+                />
+                <Form.Check
+                  required
+                  inline
+                  label="2"
+                  name="group1"
+                  type="radio"
+                  id="2"
+                />
+                <Form.Check
+                  required
+                  inline
+                  label="3"
+                  name="group1"
+                  type="radio"
+                  id="3"
+                />
+                <Form.Check
+                  required
+                  inline
+                  label="4"
+                  name="group1"
+                  type="radio"
+                  id="4"
+                />
+                <Form.Check
+                  required
+                  inline
+                  label="5"
+                  name="group1"
+                  type="radio"
+                  id="5"
+                />
+              </Form.Group>
+            </div>
+            <br />
+            <Form.Label>Choose Teacher</Form.Label>
+            <Form.Control as="select" name='teacher'>
+              {teachers.map((teacher) => {
+                return (
+                  <option id={teacher.doc_id}>
+                    {teacher.firstName} {teacher.lastName}
+                  </option>
+                );
+              })}
+              {/* <option>Choose...</option>
+            <option>...</option> */}
+            </Form.Control>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
         </Modal.Body>
       </Modal>
     </Tab.Container>
