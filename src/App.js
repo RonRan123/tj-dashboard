95eb3927d9bdf992703d142c64b268c218c4c0e1
Cooper DeVito <ced3rq@virginia.edu>
2021-06-02 16:50:30 -0400

    calendar

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React from 'react';
 import AdminDash from './components/AdminDash.js';
 import TeacherDash from './components/TeacherDash.js';
 import StudentDash from './components/StudentDash.js';
-import Home from "./components/Home.js"
+import Home from './components/Home.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Calendar from './components/Calendar';
 
 function App() {
-  
-  
-  return (
-    <div style={{ textAlign: 'center' }}>
-      <Home/>
-    </div>
-  );
+	return (
+		<div style={{ textAlign: 'center' }}>
+			<Home />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Calendar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Calendar() {
+	return (
+		<iframe
+			src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23F6BF26&amp;ctz=America%2FNew_York&amp;src=amNicWhtcGJlcXQxbWVqMThiZGRobzNibG9AZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23F4511E&amp;color=%230B8043&amp;title=TJ%20Elementary%20Calendar"
+			//style="border:solid 1px #777"
+			width="800"
+			height="600"
+			frameborder="0"
+			scrolling="no"
+		></iframe>
+	);
+}
+
+export default Calendar;
