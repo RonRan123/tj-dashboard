21a94731838f8925d9eee5bcc2088095f4745037
T-Cal <tcalcuttawala@gmail.com>
2021-06-04 11:40:48 -0400

    fixed issues with teacher admin dash

--- src/components/AdminDash.js
+++ src/components/AdminDash.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect, useContext } from 'react';
 import ClassDash from './ClassDash';
 import StudentDash from './StudentDash';
-import {Container, Row, Col} from 'react-bootstrap';
-import {ClassContext, StudentContext, TeacherContext} from './Home';
+import { Container, Row, Col } from 'react-bootstrap';
+import { ClassContext, StudentContext, TeacherContext } from './Home';
+import ClassCard from './ClassCard';
+import TeacherAdminDash from './TeacherAdminDash';
+
 function AdminDash() {
-	const {classes, getMyClasses} = React.useContext(ClassContext);
-	const {teachers, getTeachers} = React.useContext(TeacherContext);
+	const { classes, getMyClasses } = React.useContext(ClassContext);
+	const { teachers, getTeachers } = React.useContext(TeacherContext);
 	const [classID, setClassID] = useState('allIDs');
-	
+
 	// const [classes, setClasses] = useState([]);
 	// const [teachers, setTeachers] = useState([]);
 	// const getMyClasses = async () => {
@@ -31,14 +34,19 @@ function AdminDash() {
 	}, []);
 	return (
 		<div>
-			<Container>
+			<Container style={{}}>
 				<Row>
-					<Col>
-					{/* <div style={{ maxHeight: 'max-content', maxWidth: '25%', margin: '1%' }}> */}
-						<ClassDash setClassID={setClassID}/>
+					<Col md='auto' style={{margin:'auto'}}>
+						{/* <div style={{ maxHeight: 'max-content', maxWidth: '25%', margin: '1%' }}> */}
+						<ClassDash setClassID={setClassID} />
 					</Col>
-					<Col xs={10}>
-						<StudentDash classID={classID}/>
+
+					<Col md='auto'> 
+						<ClassCard classID={classID}> </ClassCard>
+						<StudentDash classID={classID} />
+					</Col>
+					<Col>
+					<TeacherAdminDash/>
 					</Col>
 				</Row>
 			</Container>
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,96 +1,97 @@
-import React from 'react';
-import { Container, Row, Col, Jumbotron, Card } from 'react-bootstrap';
-import Image from 'react-bootstrap/Image';
-import '../style.css';
+import React from "react";
+import { Container, Row, Col, Jumbotron, Card } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import "../style.css";
 function LandingPage() {
-	// const style = {
-	// 	color: 'white',
-	// 	backgroundImage: 'url (' + 'https://unsplash.com/photos/Uzg0pq7pDCU' + ')',
-	// };
-	return (
-		<div>
-			<div className="hero-image">
-				<div className="hero-text">
-					<h1>Thomas Jefferson Elementary </h1>
-					<p>Home of the Banana Slugs</p>
-				</div>
-			</div>
-			<Container fluid="lg">
-				<Row>
-					<Col>
-						<Jumbotron fluid style={{ float: 'left' }}></Jumbotron>
-						<br></br>
-					</Col>
-				</Row>
-				<Row>
-					<Col>
-						<h3 style={{ float: 'left' }}>
-							{' '}
-							About Thomas Jefferson Elementary{' '}
-						</h3>
+  // const style = {
+  // 	color: 'white',
+  // 	backgroundImage: 'url (' + 'https://unsplash.com/photos/Uzg0pq7pDCU' + ')',
+  // };
+  return (
+    <div>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1>Thomas Jefferson Elementary </h1>
+          <p>Home of the Banana Slugs</p>
+        </div>
+      </div>
+      <Container fluid="lg">
+        <Row>
+          <Col>
+            <Jumbotron fluid style={{ float: "left" }}></Jumbotron>
+            <br></br>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <h3 style={{ float: "left" }}>
+              {" "}
+              About Thomas Jefferson Elementary{" "}
+            </h3>
 
-						<p style={{ textAlign: 'left' }}>
-							<br></br>
-							<br></br>
-							Founded in 1806, Thomas Jefferson Elementary is one of the oldest
-							public schools in the U.S. Since its founding, TJ has been
-							committed to shaping young minds and developing true leaders.
-						</p>
-					</Col>
-					<Col>
-						<br></br>
-						<br></br>
-						<p style={{ textAlign: 'left' }}>
-							Thomas Jefferson Elementary allows its students to explore, and
-							endulge their curiousities through a variety of pursuits. At TJ,
-							all are welcome that are willing to learn.
-						</p>
-					</Col>
-				</Row>
+            <p style={{ textAlign: "left" }}>
+              <br></br>
+              <br></br>
+              At Thomas Jefferson Elementary School, we are a diverse community
+              of global citizens and lifelong learners that lead by example. We
+              believe in the power of positivity, respect, and cooperation. We
+              manifest our potential through confidence and work ethic.
+            </p>
+          </Col>
+          <Col>
+            <br></br>
+            <br></br>
+            <p style={{ textAlign: "left" }}>
+              Since its founding in 1683, Thomas Jefferson Elementary has valued
+              positivity, respect, and education. These principles paired with
+              diversity and camaraderie amongst our student set TJ above all institutions.
+            </p>
+          </Col>
+        </Row>
 
-				<Row>
-					<br></br>
-				</Row>
-				<Row>
-					<Col>
-						<Row>
-							<Col>
-								{/* <Image src="tech-pic.jpg" width="90%" fluid /> */}
-								<div className="tech-image"></div>
-								{/* <div className="tech-text">
+        <Row>
+          <br></br>
+        </Row>
+        <Row>
+          <Col>
+            <Row>
+              <Col>
+                {/* <Image src="tech-pic.jpg" width="90%" fluid /> */}
+                <div className="tech-image"></div>
+                {/* <div className="tech-text">
 									<h1>Thomas Jefferson Elementary </h1>
 									{/* <p>Home of the Banana Slugs</p> */}
-								{/* </div> */}
-							</Col>
-						</Row>
-						<Row>
-							<Col>
-								{/* <Image
+                {/* </div> */}
+              </Col>
+            </Row>
+            <Row>
+              <Col>
+                {/* <Image
 									src="kids-at-school.jpg"
 									height="80%"
 									width="90%"
 									fluid
 								/> */}
-								<div className="kids-image" />
-								{/* <div className="kids-text">
+                <div className="kids-image" />
+                {/* <div className="kids-text">
 									<h1>Student Projects </h1>
 									{/* <p>Explore the banana slugs' end of year projects</p> */}
-								{/* </div> */}
-							</Col>
-						</Row>
-					</Col>
-					<Col>
-						<Image src="new-super.jpg" fluid />{' '}
-						{/* <p style={{ textAlign: 'left' }}>
+                {/* </div> */}
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <Image src="new-super.jpg" fluid />{" "}
+            {/* <p style={{ textAlign: 'left' }}>
 							Thomas Jefferson Elementary School's new Superintendent has big
 							plans for the schools full return to in person classes next fall.
 							Stay tuned for more in the Daily Slug!
 						</p> */}
-					</Col>
-				</Row>
-			</Container>
-		</div>
-	);
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default LandingPage;
